e0a99a884d10e885eafec6dbda0f773a62dcd25a
rubenramos <rubenbaloncesto@gmail.com>
2014-03-31 13:37:20 +0200

    Añadido PSerpisEd con ArrayButton en la solución PGtkArray

---
+++ PGtkArray/PSerpisEd/ArrayButton.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace PSerpisEd
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class ArrayButton : Gtk.Bin
+	{
+		public ArrayButton ()
+		{
+			this.Build ();
+		}
+	}
+}
+
